ddee04ce193e2731df6895d31caae1c92baaa36d
serge-scl <sergesclv@gmail.com>
2021-08-07 07:34:04 +0300

    logic remake

--- valve_logic.py
+++ valve_logic.py
@@ -4,60 +4,13 @@ import time
 
 class SuctionCup:
     def __init__(self, x):
-        self.frequent = x
-
-    def valve(self):
-        pass
-
-    def hall_sensor(self):
-        pass
-
-
-class PulseGen:
-    pass
-
-
-class Valve:
-    def __init__(self, x, p):
         self.signal = x
-        self.press_gg = p
-        self.press_min = 10000
 
-    def valve_in(self):
-        if self.press_gg == 0:
-            return 0
-        elif self.press_gg > self.press_min:
-            return 1
+    def coil(self):
+        pass
 
-    def valve_out1(self):
-        if self.signal == 1 and self.valve_in() == 1:
-            return 1
-        elif self.signal == 4:
-            return 0
-        else:
-            return 0
-
-    def valve_out2(self):
-        if self.signal == 2 and self.valve_in() == 1:
-            return 1
-        elif self.signal == 4:
-            return 0
-        else:
-            return 0
-
-    def valve_res(self):
-        if self.signal == 3 and self.valve_in() == 1:
-            return 1
-        elif self.signal == 4:
-            return 0
-        else:
-            return 0
-
-    def valve_not(self):
-        if self.signal == 4:
-            return 1
-        else:
-            return 0
+    def ancr_vlv(self):
+        pass
 
 
 class AirReceiver:
@@ -92,36 +45,7 @@ class Pump:
             return 15000
 
 
-class SensHand:
-
-    def hall_sensor(self):
-        pass
-
-    def cap_touch(self):
-        pass
-
-    def voltmeter(self):
-        pass
-
-    def proximity_sensor(self):
-        pass
-
-
 class HandInGlove:
-    pass
-
-
-class ModulePress:
-
-    class SuctCupR(SuctionCup):
-        pass
-
-
-    class SuctCupL(SuctionCup):
-        pass
-
-
-class ModuleSense:
     pass
 
 
