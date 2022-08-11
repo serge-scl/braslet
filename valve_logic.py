3b9955d9dca4f7c520f2f65be7cfcf1674f67996
serge-scl <sergesclv@gmail.com>
2022-08-11 22:02:18 +0300

    valve logic

--- valve_logic.py
+++ valve_logic.py
@@ -1,55 +1,17 @@
-# logic of operation of valves of suction cups and pneumatic actuators
-import time
+# The initial pressure P0atm for the entire system is equal to atmospheric – atm.
+# Working volumes are constants.
+# In the case of pressure, this is the total volume of all actuators V12act,
+# and in the case of vacuum chambers V12vch.
+# The total Tcl runtime is the few seconds that are acceptable for the end user to remove one glove.
+# The volume of the air receivers - Vrx is the unknown target.
+# Pressure - Prx / Pvx (positive or negative) in the receivers is also an unknown desired value.
+from scipy.constants import atm
+from bt_mech_equation import pa, vch
 
-
-class SuctionCup:
-    def __init__(self, x):
-        self.signal = x
-
-    def coil(self):
-        pass
-
-    def ancr_vlv(self):
-        pass
-
-
-class AirReceiver:
-    def __init__(self, p):
-        self.pump_pow = p
-        self.tank = 0
-
-    def pressure_gauge(self):
-        if self.pump_pow < 10000:
-            return 0
-        elif self.pump_pow >= 10000:
-            while self.tank < 100000:
-                time.sleep(1)
-                print(f"gauge {self.tank}")
-                self.tank = self.tank + self.pump_pow
-        print("TurnOff")
-        return 0
-
-
-class Pump:
-    def __init__(self):
-        self.pwr = int(input("click"))
-
-    # def __call__(self, stp):
-    #     self.stop = stp
-
-    def power(self):
-        if self.pwr == 0:
-            return 0
-        elif self.pwr == 1:
-            print("Power")
-            return 15000
-
-
-class HandInGlove:
-    pass
-
+P0atm = atm
+V12act = pa.v_ring()  # m^3
+V12vch = vch.free_v_chamb() * 12
 
 if __name__ == "__main__":
-    pmp = Pump()
-    ars = AirReceiver(pmp.power())
-    ars.pressure_gauge()
+    print(f"V act {round(V12act * 1e6,1)}cm3")
+    print(f"V vacuum chamb ring{round(V12vch * 1e6, 1)}cm3")
