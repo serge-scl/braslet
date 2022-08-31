e5c2832fe078673050604eec7c8e53d77b9adeb0
serge-scl <sergesclv@gmail.com>
2022-08-31 06:16:41 +0300

    spring work equation

--- valve_logic.py
+++ valve_logic.py
@@ -6,12 +6,21 @@
 # The volume of the air receivers - Vrx is the unknown target.
 # Pressure - Prx / Pvx (positive or negative) in the receivers is also an unknown desired value.
 from scipy.constants import atm
-from bt_mech_equation import pa, vch, GasFlow
-from math import sqrt
+from bt_mech_equation import pa, vch, GasFlow, spring_f
+from math import sqrt, pi
+from bt_mech_equation import Const
 
 P0atm = atm
 V12act = pa.v_ring()  # m^3
 V12vch = vch.free_v_chamb() * 12
+
+
+def spring_da(x):
+    g = Const.stl  # shear modulus
+    d = 0.001  # spring wire diameter
+    c = (0.022 + 0.01) * 2 / pi  # spring coil diameter
+    n = 24  # number of turns
+    return (spring_f(g, d, c, n, x) * x) / 2
 
 
 class VlsFlow(GasFlow):
@@ -22,7 +31,15 @@ class VlsFlow(GasFlow):
         return gm1 * gm2
 
 
-vlsflw = VlsFlow(1.2, 0.001)
+class Valve:
+    def __call__(self, x):
+        return x / 1000
+
+
+vlv1 = Valve()
+
+
+vlsflw = VlsFlow(1.2, vlv1(1))
 
 if __name__ == "__main__":
     print(f"V act {round(V12act * 1e6,1)}cm3")
