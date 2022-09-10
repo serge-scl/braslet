53898751013f91445ce236ff0f6d07386207e562
serge-scl <sergesclv@gmail.com>
2022-09-10 21:28:47 +0300

    new computer and pause

--- valve_logic.py
+++ valve_logic.py
@@ -6,28 +6,48 @@
 # The volume of the air receivers - Vrx is the unknown target.
 # Pressure - Prx / Pvx (positive or negative) in the receivers is also an unknown desired value.
 from scipy.constants import atm
+from scipy.integrate import odeint
 from bt_mech_equation import pa, vch, GasFlow, spring_f
-from numpy import pi, sqrt
+from numpy import pi, sqrt, linspace
 from bt_mech_equation import Const
+import matplotlib.pyplot as plt
+
 
 P0atm = atm
 V12act = pa.v_ring()  # m^3
 V12vch = vch.free_v_chamb() * 12
 
 
-def spring_da(x):
+def spring_k():
     g = Const.stl  # shear modulus
     d = 0.001  # spring wire diameter
     c = (0.022 + 0.01) * 2 / pi  # spring coil diameter
     n = 24  # number of turns
-    return (spring_f(g, d, c, n, x) * x) / 2
+    return spring_f(g, d, c, n, 1)
 
-# dx/dt = (mx^2/t^2 + kx/2 + Es)/S
-# k - spring_da
+
+sp_k = spring_k()
+const = sp_k/2
+const2 = Const.rib_E * 0.0001 * 0.02
+
+# P = x(m*x/t**2 + k/2 + Es ) / S
+# k - spring_kx
 # E Const.rib_E
 # s ribbon 0.1 mm X 20 mm
-# S piston
+# S piston 10 x 20 mm
 # m 150 g
+
+def f_dxt(x, t):
+    md = 0.8
+    return x(const + const2)/0.0024
+
+
+x0 = 0.01
+tl = linspace(0, 100)
+od = odeint(f_dxt, x0, tl)
+plt.plot(tl, od)
+plt.show()
+
 
 
 class VlsFlow(GasFlow):
@@ -52,3 +72,4 @@ if __name__ == "__main__":
     print(f"V act {round(V12act * 1e6,1)}cm3")
     print(f"V vacuum chamb ring{round(V12vch * 1e6, 1)}cm3")
     print(f"old eqw {round(vlsflw.gf2() * 1e3,5)} g/sec,  new eqw {round(vlsflw.gf3() * 1e3,5)} g/sec")
+    print(f" spring {const}, ribbon {const2}")
