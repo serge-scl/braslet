863afc9488590a712a4d4db731975faf2b9243d7
sergesclv <sergesclv@gmail.com>
2022-11-01 10:58:13 +0300

    valve logic 2 physical level

---
+++ valve_ligic2.py
@@ -0,0 +1,8 @@
+# The first problem arises if, when the edge of the suction cup is poorly laid, air penetrates inside.
+# In order to avoid leakage of outside air into the system, a separate vacuum chamber is provided.
+# The inlet and outlet of the chamber locks two valve positions.
+# In the first position, the valve is pressed against the suction cup opening by a spring,
+# separating the suction cup from the vacuum chamber.
+# In this case, the hole from the vacuum line to the chamber is open.
+# In the second position, the magnet is activated, the valve armature opens
+# the connection of the vacuum chamber with the suction cup and closes the hole in line.
